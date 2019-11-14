95044bdb7cf1d392a4b27fe38fc1e6b91e54917c
Saleh <9ale7.lootah@gmail.com>
2019-11-14 18:34:03 +0000

    Sambool

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Saleh Lootah");
+        System.out.println("Saleh Sambool");
     }
 }
