6754f182eeaca6d22314c1b6e3f7f2d4a47f2d57
Saleh <9ale7.lootah@gmail.com>
2019-11-14 20:24:49 +0000

    Added possible classes, Player count semi-functional.

--- src/pebble_game/PebbleGame.java
+++ src/pebble_game/PebbleGame.java
@@ -1,7 +1,68 @@
 package pebble_game;
+import java.util.*;
 
 public class PebbleGame {
+    public static String dummyRange = "1,2,3,4,5,6,7,8,9,10";
+
+    class Pebble {
+        private String bagName;
+        private String weight;
+
+        public Pebble(String bagName, String weight){
+            this.bagName = bagName;
+            this.weight = weight;
+        }
+    }
+
+    class Bag {
+        
+    }
+    class BlackBag extends Bag {
+
+    }
+    class WhiteBag extends Bag {
+
+    }
+
+    static class Player {
+        private String name;
+
+        public Player() {
+            
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+        public String getName(){
+            return name;
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Zain is a penis haha");
+        
+        ArrayList<Player> players = new ArrayList<Player>();
+
+        System.out.println("How many players are in this game?");
+        Scanner scanner = new Scanner(System.in);
+        int playerCount = scanner.nextInt();
+
+
+        for(int i = 0; i < playerCount; i++)
+        {
+            System.out.print("What is Player " + (i + 1) + " name?\n");
+            String playerName = scanner.next();
+            Player plr = new Player();
+            plr.setName(playerName);
+            players.add(plr);
+            
+        }
+        scanner.close();
+        for (int j = 0; j < players.size(); j++){
+            System.out.println(players.get(j).getName());
+        }
+
+
     }
 }
