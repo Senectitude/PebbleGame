7363408f86575b83fabbc38e039638a964224760
Saleh <9ale7.lootah@gmail.com>
2019-11-14 18:54:14 +0000

    Renamed package

--- src/pebble_game/PebbleGame.java
+++ src/pebble_game/PebbleGame.java
@@ -1,6 +1,6 @@
-package app;
+package pebble_game;
 
-public class App {
+public class PebbleGame {
     public static void main(String[] args) throws Exception {
         System.out.println("Zain is a penis haha");
     }
